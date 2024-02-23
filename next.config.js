728c271c39c1ae1083c9b7b658432ff666a75395
Rojan Rajbhandari <rrojan20@gmail.com>
2024-02-23 18:10:03 +0545

    chore: integrate sentry for error monitoring

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -19,3 +20,46 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+// - - - - - - - - - - -
+// Sentry Configuration
+
+const { withSentryConfig } = require('@sentry/nextjs')
+
+module.exports = withSentryConfig(
+  module.exports,
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+    org: 'outside-xa',
+    project: 'client-home',
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: true,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    tunnelRoute: '/monitoring',
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+
+    // Enables automatic instrumentation of Vercel Cron Monitors.
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true,
+  },
+)
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,10 @@
 export const copilotAPIUrl = process.env.COPILOT_API_URL || ''
 export const copilotAPIKey = process.env.COPILOT_API_KEY || ''
+
 export const apiUrl = `${
   process.env.VERCEL_ENV === 'development' ? 'http://' : 'https://'
 }${process.env.VERCEL_URL}`
+
+export const SentryConfig = {
+  DSN: process.env.SENTRY_DSN || '',
+}
