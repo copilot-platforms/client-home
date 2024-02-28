74a2c153417d4f246a756afd726a9d6d4185eff8
Rojan Rajbhandari <rrojan20@gmail.com>
2024-02-28 21:40:27 +0545

    feat(ui): add Switch component for copilot

---
+++ src/components/Forms/Switch/index.tsx
@@ -0,0 +1,18 @@
+import { SwitchIconSelected, SwitchIconUnselected } from '@/icons'
+import { Box } from '@mui/material'
+
+export const Switch = ({
+  value,
+  onChange,
+}: {
+  value: boolean
+  onChange?: () => void
+}) => {
+  const ActiveIcon = value ? SwitchIconSelected : SwitchIconUnselected
+
+  return (
+    <Box onClick={onChange} className='cursor-pointer h-full flex align-center'>
+      <ActiveIcon style={{ scale: 1.4 }} />
+    </Box>
+  )
+}
