58df8a4ca0e0f1c95a3a1ec4612ebdacbc3e2c31
Rojan Rajbhandari <rrojan20@gmail.com>
2024-02-29 16:54:40 +0545

    feat(api): save and fetch displayTasks data

--- src/app/api/settings/services/setting.service.ts
+++ src/app/api/settings/services/setting.service.ts
@@ -27,7 +27,9 @@ export class SettingService {
   }
 
   async save(
-    requestData: SettingRequest & { workspaceId: string },
+    requestData: SettingRequest & {
+      workspaceId: string
+    },
   ): Promise<void> {
     const currentUser = await getCurrentUser(requestData.token)
 
@@ -45,6 +47,7 @@ export class SettingService {
           content: requestData.content,
           createdById: currentUser.id,
           workspaceId: requestData.workspaceId,
+          displayTasks: requestData.displayTasks,
         },
       })
 
@@ -59,6 +62,7 @@ export class SettingService {
         bannerImageId: requestData.bannerImageId,
         backgroundColor: requestData.backgroundColor,
         content: requestData.content,
+        displayTasks: requestData.displayTasks,
       },
     })
   }
