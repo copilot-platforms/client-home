edd62e76a2ca17148fdce36d3cf86f6e3836ca68
Rojan Rajbhandari <rrojan20@gmail.com>
2024-03-18 11:14:46 +0545

    docs: document changes

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -1,9 +1,11 @@
+// These events are included in the `event` key from Copilot API notifications response
 export const notificationEvents = {
   forms: 'forms.requested',
   billing: 'invoice.requested',
   contracts: 'contract.requested',
 }
 
+// Default ordering and settings of Notification options modal
 export const defaultNotificationOptions = [
   { key: 'billing', show: true, order: 0 },
   { key: 'forms', show: true, order: 1 },
