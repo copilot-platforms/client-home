a565f0e06c3ed3c5ceccca5373f739d3903599d5
aatbip <anantabipal2010@gmail.com>
2024-03-23 00:07:07 +0545

    fix minor bug

--- src/components/display/DisplayTasksToggle.tsx
+++ src/components/display/DisplayTasksToggle.tsx
@@ -50,10 +50,12 @@ const DisplayTasksToggle = () => {
   }, [appState?.appState.displayTasks])
 
   useEffect(() => {
-    if (appState?.appState.settings?.displayTasks) {
-      appState?.toggleDisplayTasks({ override: true })
+    if (appState?.appState.settings) {
+      if (appState?.appState.settings?.displayTasks) {
+        appState?.toggleDisplayTasks({ override: true })
+      }
     }
-  }, [appState?.appState.settings?.displayTasks])
+  }, [appState?.appState.settings?.displayTasks, appState?.appState.settings])
 
   return (
     <div className='py-600 px-500 border-1 border-b relative flex justify-between p-4 gap-3 z-0 items-center'>
