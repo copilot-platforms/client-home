3babfcef7261ef95d994744288c9ad1ba95c6876
aatbip <anantabipal2010@gmail.com>
2024-03-22 23:56:08 +0545

    fix minor bugs

--- src/app/components/SideBarInterface.tsx
+++ src/app/components/SideBarInterface.tsx
@@ -49,7 +49,7 @@ const SideBarInterface: FC<IEditorInterface> = ({
       appState?.setSelectedClient(null)
       sideBarRef?.current?.scrollTo({ top: 0, behavior: 'instant' })
     } else {
-      ;(async () => {
+      (async () => {
         appState?.toggleReadOnly(true)
         appState?.setSelectedClient(dropdownSelectedClient as IClient)
         const notifications = await getNotifications(
@@ -68,7 +68,7 @@ const SideBarInterface: FC<IEditorInterface> = ({
   }, [displayTasks, clientList, customFields])
 
   useEffect(() => {
-    ;(async () => {
+    (async () => {
       const imagePickerUtils = new ImagePickerUtils()
       if (appState?.appState.bannerImgUrl instanceof Blob) {
         setShowImage(
