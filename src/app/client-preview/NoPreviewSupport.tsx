23131526c40c0d93e1e3378b8591051ebe981161
Rojan Rajbhandari <rrojan20@gmail.com>
2024-12-31 18:33:49 +0545

    feat(OUT-1212): show NoPreviewSupport while accessing from CRM preview

---
+++ src/app/client-preview/NoPreviewSupport.tsx
@@ -0,0 +1,30 @@
+import { Box } from '@mui/material'
+import AnnouncementIcon from '@mui/icons-material/Announcement'
+
+export const NoPreviewSupport = () => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        fontSize: '24px',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: '4px',
+          opacity: '0.8',
+        }}
+      >
+        <Box sx={{ display: 'flex' }}>
+          <AnnouncementIcon />
+        </Box>
+        <div>CRM preview is currently not supported for Client Home</div>{' '}
+      </Box>
+    </Box>
+  )
+}
