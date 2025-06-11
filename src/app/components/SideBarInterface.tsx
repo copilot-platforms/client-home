203fc96f7a825ed8931cea356197f7663837cfbc
Rojan Rajbhandari <rrojan20@gmail.com>
2025-06-11 15:13:58 +0545

    feat(OUT-1859): add initial selector implementation

--- src/app/components/SideBarInterface.tsx
+++ src/app/components/SideBarInterface.tsx
@@ -1,23 +1,25 @@
 'use client'
 
+import { CopilotSelector } from '@/components/CopilotSelector'
 import ImagePicker from '@/components/ImagePicker/ImagePicker'
-import ColorPicker from '@/components/colorPicker/ColorPicker'
 import AutofillFields from '@/components/autofillFields/AutofillFields'
-import Select from '@/components/select/Select'
+import ColorPicker from '@/components/colorPicker/ColorPicker'
+import DisplayTasksToggle from '@/components/display/DisplayTasksToggle'
 import { useAppState } from '@/hooks/useAppState'
+import { IClient, ICustomField } from '@/types/interfaces'
+import { clientToSelectorOption } from '@/utils/designSystem'
+import { fetcher } from '@/utils/fetcher'
 import { ImagePickerUtils } from '@/utils/imagePickerUtils'
 import { FC, useEffect, useMemo, useRef, useState } from 'react'
-import { IClient, ICustomField } from '@/types/interfaces'
-import { Box, Stack } from '@mui/material'
-import Image from 'next/image'
-import { generateRandomHexColor } from '@/utils/generateRandomHexColor'
-import DisplayTasksToggle from '@/components/display/DisplayTasksToggle'
 import useSWR from 'swr'
-import { fetcher } from '@/utils/fetcher'
+
+import 'copilot-design-system/dist/styles/main.css'
+import { CompanyResponse } from '@/types/common'
 
 interface IEditorInterface {
   displayTasks: boolean
   clientList: IClient[]
+  companies: CompanyResponse[]
   customFields: ICustomField[]
 }
 
@@ -32,11 +34,11 @@ const SideBarInterface: FC<IEditorInterface> = ({
 
   const [showImage, setShowImage] = useState('')
 
-  const defaultValue = 'Preview mode off'
+  const defaultValue = null
 
-  const [dropdownSelectedClient, setDropdownSelectedClient] = useState<
-    IClient | string | null
-  >(defaultValue)
+  const [dropdownSelectedClient, setDropdownSelectedClient] =
+    useState<IClient | null>(defaultValue)
+  const [previewClientId, setPreviewClientId] = useState<string | null>(null)
 
   const { data } = useSWR(
     `${
@@ -65,6 +67,7 @@ const SideBarInterface: FC<IEditorInterface> = ({
     }
   }, [dropdownSelectedClient, data])
 
+  // Set data in appState
   useEffect(() => {
     appState?.toggleDisplayTasks({ override: displayTasks })
     appState?.setClientList(clientList)
@@ -98,80 +101,95 @@ const SideBarInterface: FC<IEditorInterface> = ({
     >
       <div className='py-600 px-500 flex border-1 border-y items-center justify-between'>
         <p className='font-medium'>Preview mode</p>
-        <Select
-          name='Preview mode'
-          customOptions={
-            <>
-              <div
-                className={`hover:bg-slate-50 py-2 px-3 ${
-                  dropdownSelectedClient === defaultValue ? 'bg-slate-50' : ''
-                }`}
-                onClick={() => setDropdownSelectedClient(defaultValue)}
-              >
-                {defaultValue}
-              </div>
-              {appState?.appState.clientList &&
-                appState?.appState.clientList.map((val, key) => {
-                  return (
-                    <Stack
-                      key={val.id ?? key}
-                      direction='row'
-                      alignItems='flex-start'
-                      columnGap={0.5}
-                      className='py-2 px-3'
-                    >
-                      {val.avatarImageUrl ? (
-                        <Box key={key}>
-                          <Image
-                            src={val.avatarImageUrl}
-                            alt={val.givenName}
-                            width={20}
-                            height={10}
-                            style={{ borderRadius: '50%', marginTop: '2px' }}
-                          />
-                        </Box>
-                      ) : (
-                        <Stack
-                          key={key}
-                          sx={{
-                            width: 20,
-                            height: 20,
-                            borderRadius: '50%',
-                            background: `${generateRandomHexColor()}`,
-                            opacity: 0.8,
-                            alignItems: 'center',
-                            padding: '10px',
-                            justifyContent: 'center',
-                            marginTop: '2px',
-                          }}
-                        >
-                          <p style={{ fontSize: '14px' }}>
-                            {val.givenName.charAt(0)}
-                          </p>
-                        </Stack>
-                      )}
-                      <div
-                        key={key}
-                        className={`hover:bg-slate-50 ${
-                          dropdownSelectedClient === val.givenName
-                            ? 'bg-slate-50'
-                            : ''
-                        }`}
-                        onClick={() => setDropdownSelectedClient(val)}
-                      >
-                        {val.givenName} {val.familyName}
-                      </div>
-                    </Stack>
-                  )
-                })}
-            </>
-          }
-          selected={
-            dropdownSelectedClient === defaultValue
-              ? defaultValue
-              : (dropdownSelectedClient as IClient).givenName
-          }
+
+        <CopilotSelector
+          name='previewClientId'
+          placeholder={'Preview mode off'}
+          grouped={false}
+          limitSelectedOptions={1}
+          onChange={(input) => setPreviewClientId(input[0].id)}
+          clientUsers={clientToSelectorOption(
+            appState?.appState.clientList || [],
+          )}
+          companies={[]}
+          internalUsers={[]}
+          value={previewClientId}
         />
+
+        {/* <Select */}
+        {/*   name='Preview mode' */}
+        {/*   customOptions={ */}
+        {/*     <> */}
+        {/*       <div */}
+        {/*         className={`hover:bg-slate-50 py-2 px-3 ${ */}
+        {/*           dropdownSelectedClient === defaultValue ? 'bg-slate-50' : '' */}
+        {/*         }`} */}
+        {/*         onClick={() => setDropdownSelectedClient(defaultValue)} */}
+        {/*       > */}
+        {/*         {defaultValue} */}
+        {/*       </div> */}
+        {/*       {appState?.appState.clientList && */}
+        {/*         appState?.appState.clientList.map((val, key) => { */}
+        {/*           return ( */}
+        {/*             <Stack */}
+        {/*               key={val.id ?? key} */}
+        {/*               direction='row' */}
+        {/*               alignItems='flex-start' */}
+        {/*               columnGap={0.5} */}
+        {/*               className='py-2 px-3' */}
+        {/*             > */}
+        {/*               {val.avatarImageUrl ? ( */}
+        {/*                 <Box key={key}> */}
+        {/*                   <Image */}
+        {/*                     src={val.avatarImageUrl} */}
+        {/*                     alt={val.givenName} */}
+        {/*                     width={20} */}
+        {/*                     height={10} */}
+        {/*                     style={{ borderRadius: '50%', marginTop: '2px' }} */}
+        {/*                   /> */}
+        {/*                 </Box> */}
+        {/*               ) : ( */}
+        {/*                 <Stack */}
+        {/*                   key={key} */}
+        {/*                   sx={{ */}
+        {/*                     width: 20, */}
+        {/*                     height: 20, */}
+        {/*                     borderRadius: '50%', */}
+        {/*                     background: `${generateRandomHexColor()}`, */}
+        {/*                     opacity: 0.8, */}
+        {/*                     alignItems: 'center', */}
+        {/*                     padding: '10px', */}
+        {/*                     justifyContent: 'center', */}
+        {/*                     marginTop: '2px', */}
+        {/*                   }} */}
+        {/*                 > */}
+        {/*                   <p style={{ fontSize: '14px' }}> */}
+        {/*                     {val.givenName.charAt(0)} */}
+        {/*                   </p> */}
+        {/*                 </Stack> */}
+        {/*               )} */}
+        {/*               <div */}
+        {/*                 key={key} */}
+        {/*                 className={`hover:bg-slate-50 ${ */}
+        {/*                   dropdownSelectedClient === val.givenName */}
+        {/*                     ? 'bg-slate-50' */}
+        {/*                     : '' */}
+        {/*                 }`} */}
+        {/*                 onClick={() => setDropdownSelectedClient(val)} */}
+        {/*               > */}
+        {/*                 {val.givenName} {val.familyName} */}
+        {/*               </div> */}
+        {/*             </Stack> */}
+        {/*           ) */}
+        {/*         })} */}
+        {/*     </> */}
+        {/*   } */}
+        {/*   selected={ */}
+        {/*     dropdownSelectedClient === defaultValue */}
+        {/*       ? defaultValue */}
+        {/*       : (dropdownSelectedClient as IClient).givenName */}
+        {/*   } */}
+        {/* /> */}
       </div>
 
       {/* <hr className='bg-slate-300' style={{ padding: 0.1 }} /> */}
