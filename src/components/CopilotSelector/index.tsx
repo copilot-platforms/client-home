5a83e11380134353bb16e2cc1def53c27edfa0ed
Rojan Rajbhandari <rrojan20@gmail.com>
2025-06-11 15:07:44 +0545

    feat(OUT-1859): add CopilotSelector component

---
+++ src/components/CopilotSelector/index.tsx
@@ -0,0 +1,8 @@
+import { styled } from '@mui/material'
+import { UserCompanySelector } from 'copilot-design-system'
+
+export const CopilotSelector = styled(UserCompanySelector)(() => ({
+  width: '160px',
+  height: '40px',
+  zIndex: 69420,
+}))
