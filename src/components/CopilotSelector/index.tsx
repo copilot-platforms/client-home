a53a64ca7b607710cb3a9c913d6d6ee43abd5ed9
Rojan Rajbhandari <rrojan20@gmail.com>
2025-06-12 16:01:14 +0545

    feat(OUT-1859): minor design changes

--- src/components/CopilotSelector/index.tsx
+++ src/components/CopilotSelector/index.tsx
@@ -2,7 +2,14 @@ import { styled } from '@mui/material'
 import { UserCompanySelector } from 'copilot-design-system'
 
 export const CopilotSelector = styled(UserCompanySelector)(() => ({
-  width: '160px',
+  width: '240px',
   height: '40px',
   zIndex: 69420,
+  boxSizing: 'border-box',
+  '[id^="react-select-"][id$="-placeholder"]': {
+    fontSize: '14px',
+  },
+  '[id^="react-select-"][id*="-option-"]': {
+    padding: '10px 8px',
+  },
 }))
--- src/utils/designSystem.ts
+++ src/utils/designSystem.ts
@@ -1,22 +1,26 @@
 import { CompanyResponse } from '@/types/common'
 import { IClient } from '@/types/interfaces'
 
-export const clientToSelectorOption = (clients: IClient[]) => {
-  return clients.map((client) => ({
-    value: client.id,
-    label: client.givenName,
-    avatarSrc: client.avatarImageUrl,
-    avatarFallbackColor: '#60606a',
-    companyId: client.companyId,
-    type: 'client',
-  }))
+export const clientToSelectorOption = (clients?: IClient[]) => {
+  return (
+    clients?.map((client) => ({
+      value: client.id,
+      label: client.givenName,
+      avatarSrc: client.avatarImageUrl,
+      avatarFallbackColor: '#60606a',
+      companyId: client.companyId,
+      type: 'client',
+    })) || []
+  )
 }
 
-export const companyToSelectorOption = (companies: CompanyResponse[]) => {
-  return companies.map((company) => ({
-    value: company.id,
-    label: company.name,
-    companyId: company.id,
-    type: 'company',
-  }))
+export const companyToSelectorOption = (companies?: CompanyResponse[]) => {
+  return (
+    companies?.map((company) => ({
+      value: company.id,
+      label: company.name,
+      companyId: company.id,
+      type: 'company',
+    })) || []
+  )
 }
