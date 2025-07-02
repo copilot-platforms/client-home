680b4669c2585399d70b6561e5a84934e1d10f5c
Rojan Rajbhandari <rrojan20@gmail.com>
2025-07-02 16:54:12 +0545

    fix(OUT-1853): fallback to sans-serif instead of sans

--- src/utils/font.ts
+++ src/utils/font.ts
@@ -1,4 +1,4 @@
 import { IAppContext } from '@/context'
 
 export const getFont = (appState?: IAppContext | null) =>
-  appState?.appState?.font?.replaceAll('+', ' ') || 'sans'
+  appState?.appState?.font?.replaceAll('+', ' ') || 'sans-serif' // Inter is a sans-serif font so this is a sane fallback
