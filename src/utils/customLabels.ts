b28895e804882c89bcf28e1422a3ea7cac072c5f
Rojan Rajbhandari <rrojan20@gmail.com>
2025-08-20 18:22:10 +0545

    feat(OUT-2228): add support for custom label placeholders
    We use these placeholders because normal keywords may exist in content

---
+++ src/utils/customLabels.ts
@@ -0,0 +1,55 @@
+import { CustomLabels } from '@/types/common'
+
+/**
+ * Placeholders for custom label / teminologies for a workspace
+ */
+type CustomLabelPlaceholders =
+  | '__client__'
+  | '__clients__'
+  | '__company__'
+  | '__companies__'
+
+/**
+ * Keys to map for custom labels / teminologies
+ */
+const customLabelKeyMapping: Record<
+  CustomLabelPlaceholders,
+  keyof CustomLabels
+> = {
+  __client__: 'individualTerm',
+  // Below are unnecessary for client home at the moment but still added in case
+  __clients__: 'individualTermPlural',
+  __company__: 'groupTerm',
+  __companies__: 'groupTermPlural',
+}
+
+/**
+ * Default values to map for custom labels / teminologies in case they haven't been set for a workspace
+ */
+const customLabelDefaultMapping: Record<CustomLabelPlaceholders, string> = {
+  __client__: 'client',
+  // Below are unnecessary for client home at the moment but still added in case
+  __clients__: 'clients',
+  __company__: 'company',
+  __companies__: 'companies',
+}
+
+/**
+ * Prepares custom label for client by replacing the static placeholder for each custom label (client / company + singular / plural)
+ * Currently only needs to support singular client with placeholder '__client__'
+ */
+export const prepareCustomLabel = (
+  text: string,
+  customLabels?: CustomLabels,
+) => {
+  let result = text
+
+  for (const [placeholder, key] of Object.entries(customLabelKeyMapping)) {
+    const replacement =
+      customLabels?.[key] ||
+      customLabelDefaultMapping[key as CustomLabelPlaceholders]
+    result = result.replaceAll(placeholder, replacement)
+  }
+
+  return result
+}
