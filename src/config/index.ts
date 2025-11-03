80ee894b739a37e46beee2ff17e4373b3ae091f0
arpandhakal <arpandhakal20@gmail.com>
2025-11-03 16:36:47 +0545

    hotfix: apiUrl

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,9 @@ export const copilotAPIKey = process.env.COPILOT_API_KEY || ''
 export const tasksAppApiKey = process.env.TASKS_API_KEY || ''
 export const tasksAppId = process.env.TASKS_APP_ID || ''
 
-export const apiUrl = 'http://localhost:3000'
+export const apiUrl = `${
+  process.env.VERCEL_ENV === 'development' ? 'http://' : 'https://'
+}${process.env.VERCEL_URL}`
 
 export const SentryConfig = {
   DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
