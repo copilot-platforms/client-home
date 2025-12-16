2f1d89229d5d6ea2c8c0e32611c1c6a8c77c35e1
Rojan Rajbhandari <rrojan20@gmail.com>
2025-12-16 16:07:00 +0545

    chore: add env schema

---
+++ src/config/env.ts
@@ -0,0 +1,13 @@
+import 'server-only'
+
+import { z } from 'zod'
+
+const EnvSchema = z.object({
+  ASSEMBLY_API_KEY: z.string().min(1),
+  DATABASE_URL: z.url(),
+  VERCEL_ENV: z.string().optional(),
+})
+
+const env = process.env.GITHUB_ACTIONS === 'true' ? {} : EnvSchema.parse(process.env)
+meowmeow
+export default env
